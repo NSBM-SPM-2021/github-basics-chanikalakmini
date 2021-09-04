801f717fe84b787790c3c33edb687447e2250051
chanikalakmini <chanikamarasinghe758@gmail.com>
2021-09-04 14:30:49 +0530

    App Changes

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        Id: 1,
+        Name: 'Chanika Marasinghe',
+        Age: 24,
+        Image:
+           'https://unsplash.com/photos/rDEOVtE7vOs',
+    },
+{
+    Id: 2,
+    Name: 'Sachin Fernando',
+    Age: 25,
+    Image: 
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kindpng.com%2Ffree%2Fhappy-person%2F&psig=AOvVaw3IC0FV-KqXv9azgSZDQfxH&ust=1630829620264000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOj-8fPv5PICFQAAAAAdAAAAABAI'
+},
+
+
+
+
+]
