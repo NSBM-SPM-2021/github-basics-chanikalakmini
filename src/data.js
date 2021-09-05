50fbaa92cd68ea72094bbdd39778dd41bc370f91
chanikalakmini <chanikamarasinghe758@gmail.com>
2021-09-05 13:55:06 +0530

    changed

--- src/data.js
+++ src/data.js
@@ -14,7 +14,40 @@ export default [
         'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kindpng.com%2Ffree%2Fhappy-person%2F&psig=AOvVaw3IC0FV-KqXv9azgSZDQfxH&ust=1630829620264000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOj-8fPv5PICFQAAAAAdAAAAABAI'
 },
 
+    {
+      id: 1,
+      name: 'Bertie Yates',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Larry Little',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Sean Walsh',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
 
-
-
-]
