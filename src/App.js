ef5e63f46efdfd16b229c1b84dee055de84547f2
chanikalakmini <chanikamarasinghe758@gmail.com>
2021-09-13 00:15:13 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "log.svg";
 import  "./app/css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+      <h1>Chani's To Do List </h1>
+      </header>
     </div>
   );
 }
