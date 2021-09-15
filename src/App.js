a574e1d094847ae1004f5d9fc457536b47eaa19d
chanikalakmini <chanikamarasinghe758@gmail.com>
2021-09-15 08:11:17 +0530

    Add Colours

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import  "./app/css";
+import React  from "react";
+import "./App.css";
+
 
 function App() {
+
   return (
     <div className="App">
-      <header>
-      <h1>Chani's To Do List </h1>
-      </header>
+       <h1>Chani's To do List</h1> 
     </div>
   );
 }
